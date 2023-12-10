412a1fb80e6b643727a4439f278dc1a032cf98f9
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2023-12-10 14:36:18 +0530

    commit parent-node traversal

--- relationship.js
+++ relationship.js
@@ -11,11 +11,29 @@
 
 let ul =document.querySelector('ul');
 console.log(ul);
-console.log(ul.parentNode);
-console.log(ul.parentElement);
+/*
+parentNode:
+    * The parentNode property returns the parent node of the specified node, including elements, text nodes, and comment nodes.
+    * It's a generic property that gives you the immediate parent node, regardless of its type.
+*/
+console.log(ul.parentNode); // outputs=> div.container
+console.log(ul.parentNode.parentNode); //outputs=> body
+/*
+parentElement:
+    * The parentElement property returns the parent element of the specified element.
+    * Unlike parentNode, parentElement specifically refers to an HTML element node. 
+    * It returns null if the parent node is not an element. 
+*/
+console.log(ul.parentElement); //outputs=> div.container
+console.log(ul.parentElement.parentElement); //outputs=> body
 
 //Child Node Traversal
 
+console.log(ul.childNodes); //outputs=> NodeList(9) [#text, li.list-items, #text, li.list-items, #text, li.list-items, #text, li.list-items, #text] 
+console.log(ul.firstChild);
+console.log(ul.lastChild);
 
+ul.childNodes[1].style.backgroundColor='blue';
 
+console.log(ul.firstElementChild);
 //Sibling Node Traversal
