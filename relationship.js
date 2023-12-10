5e1356b67c5f2c14cfa8f44eefaf1762d7bd232f
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2023-12-10 13:48:08 +0530

    commit relationship.js

---
+++ relationship.js
@@ -0,0 +1,21 @@
+//DoM Manupulation
+
+
+
+//Traverse the DOM
+
+
+
+
+//Parent Node Traversal
+
+let ul =document.querySelector('ul');
+console.log(ul);
+console.log(ul.parentNode);
+console.log(ul.parentElement);
+
+//Child Node Traversal
+
+
+
+//Sibling Node Traversal
