20bcefec8149c44b3d6d4f818a36e2056a0d4368
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2023-12-11 09:21:48 +0530

    commit eventListener

--- eventListener.js
+++ eventListener.js
@@ -26,4 +26,21 @@ document.addEventListener("DOMContentLoaded", function() {
         newBackgroundColor.style.backgroundColor='blue';
     }
     newBackgroundColor.addEventListener("mouseover",changeBgColor);
+
+
+    //Reveal Event
+
+    const revealBtn = document.querySelector('.reveal-btn');
+    const hiddenContent =document.querySelector('.hidden-content');
+    function revealContent(){
+        /*if (hiddenContent.classList.contains('reveal-btn')){
+            hiddenContent.classList.remove('reveal-btn');
+        }else{
+           hiddenContent.classList.add('reveal-btn'); 
+        }*/
+        hiddenContent.classList.toggle('.reveal-btn');
+    }
+    revealBtn.addEventListener("click",revealContent);
+
+
 });
